50b38f616f2e39bc91c6730586a947c925f1a066
erkanturk <refres98@gmail.com>
2025-11-06 16:26:19 +0300

    Add project files.

---
+++ ETICARET/ETICARET.DataAccess/Abstract/IRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETICARET.DataAccess.Abstract
+{
+    public interface IRepository<T>
+    {
+        T GetById(int id);//Belirtilen ıd ye sahip tek bir nesneyi getirir
+        T GetOne(Expression<Func<T, bool>> filter = null);//Belirtilen koşula uyan yek bir nesneyi getirir
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);//Belirtile koşula uyan tüm nesneleri getirir
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+    /* 
+     * Bu yapı tüm veri erişim katmanlarında olan ortak Crud işlemlerini tek bir yerden yönetilmesini sağlayacak
+     * Generic olması sayesinde Product,Category,Order gibi farklı nesneler için tekrar tekrar yazılmak zorunda kalmayacak.
+     
+     
+     */
+}
---
+++ ETICARET/ETICARET.Entities/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETICARET.Entities
+{
+    public class Cart
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+    }
+    public class CartItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }//Foreign key ilişkisi
+        public Product Product { get; set; } //Navigator property
+        public Cart Cart { get; set; }
+        public int CartId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
