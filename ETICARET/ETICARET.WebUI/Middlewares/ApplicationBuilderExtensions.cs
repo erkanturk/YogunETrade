c760d1441bff15cbb33af4c5131414377340abce
erkanturk <refres98@gmail.com>
2025-11-12 15:02:40 +0300

    home

--- ETICARET/ETICARET.WebUI/Middlewares/ApplicationBuilderExtensions.cs
+++ ETICARET/ETICARET.WebUI/Middlewares/ApplicationBuilderExtensions.cs
@@ -1,7 +1,19 @@
-﻿namespace ETICARET.WebUI.Middlewares
-{
-    public class ApplicationBuilderExtensions
-    {
+﻿using Microsoft.Extensions.FileProviders;
 
+namespace ETICARET.WebUI.Middlewares
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "node_modules");
+            var options = new StaticFileOptions()
+            {
+                FileProvider=new PhysicalFileProvider(path),
+                RequestPath="/modules"
+            };
+            app.UseStaticFiles(options);
+            return app;
+        }
     }
 }
