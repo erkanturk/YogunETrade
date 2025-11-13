4cdc8215bdc9532edc6fbf0109b1096c23cce251
erkanturk <refres98@gmail.com>
2025-11-13 15:20:09 +0300

    AccountController Register View

--- ETICARET/ETICARET.WebUI/Extensions/TempDataExtensions.cs
+++ ETICARET/ETICARET.WebUI/Extensions/TempDataExtensions.cs
@@ -7,13 +7,14 @@ namespace ETICARET.WebUI.Extensions
     {
         public static void Put<T>(this ITempDataDictionary tempData, string key, T value) where T : class
         {
-            tempData[key]=JsonConvert.SerializeObject(value);
+            tempData[key] = JsonConvert.SerializeObject(value);
         }
+
         public static T Get<T>(this ITempDataDictionary tempData, string key) where T : class
         {
             object o;
             tempData.TryGetValue(key, out o);
-            return o==null ? null : JsonConvert.DeserializeObject<T>((string)o);
+            return o == null ? null : JsonConvert.DeserializeObject<T>((string)o);
         }
     }
 }
